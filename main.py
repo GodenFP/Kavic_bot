d63f2eaa0460849e3bbbd53571dec6a27c5dd2b6
GodenFP <ak9806753@gmail.com>
2021-03-23 17:57:38 +0800

    v1.1.1 update
    order bot big fix

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ dt = datetime.datetime
 class Kavic_Bot(Client):
 
     order_open = False
+    now_shop = []
 
-    num_weekday = ['週 一', '週 二', '週 三', '週 四', '週 五']
     
     def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
         '''self.markAsDelivered(thread_id, message_object.uid)
@@ -54,7 +54,7 @@ class Kavic_Bot(Client):
                 for title in M[M.find(' ') + 1:].split(','):
                     if title.startswith('https://www.youtube.com/'):
                         l.append(title)
-                    else:
+                    elif title != '':
                         url = get_url_by_title(title)
                         if M.startswith('-a ') or M.startswith('add '):
                             l.append(url)
@@ -65,7 +65,7 @@ class Kavic_Bot(Client):
 
                 with open('song_list.txt', 'w') as song_list:
                     for line in l:
-                        song_list.write(line + '\n')
+                        song_list.write('\n' + line)
                         
                 #num of sent songs
                 if M.startswith('-a ') or M.startswith('add '):
@@ -91,9 +91,12 @@ class Kavic_Bot(Client):
             if M.startswith('課表'):
                 wb = openpyxl.open('Source' + sep + 'class_sheet.xlsx')
                 sheet = wb.worksheets[0]
+
+                num_weekday = ['週 一', '週 二', '週 三', '週 四', '週 五']
+                
                 if len(M.split()) > 1:
                     wkday = int(M.split()[1]) - 1
-                    self.send(Message(self.num_weekday[wkday] + ' 課 表'), thread_id = thread_id, thread_type = thread_type)
+                    self.send(Message(num_weekday[wkday] + ' 課 表'), thread_id = thread_id, thread_type = thread_type)
                 else:
                     wkday = dt.weekday(dt.now())
                     self.send(Message(u'今 日 課 表'), thread_id = thread_id, thread_type = thread_type)
@@ -128,42 +131,60 @@ class Kavic_Bot(Client):
 
                 text_list = M.split()
                 #check whether the command format is right or not
-                if len(text_list) > 1 and len(text_list) < 5:
+                if len(text_list) > 1 :#and len(text_list) < 5:
                     #order commands that only admin can use
                     if author_id == self.uid:
                         if text_list[1] == 'open':
                             self.order_open = True
+                            shop_name = M.split(' ', 2)[2]
                     
-                            self.send(Message(u'現正訂購 : ' + M.split()[2]), thread_id = thread_id, thread_type = thread_type)
-                            self.sendLocalFiles('Source' + sep + pc[text_list[2]], message = Message(u'品項:'), thread_id = thread_id, thread_type = thread_type)
+                            self.send(Message(u'現正訂購 : ' + shop_name), thread_id = thread_id, thread_type = thread_type)
+                            self.sendLocalFiles('Source' + sep + 'shop' + sep + pc[shop_name], message = Message(u'品項:'), thread_id = thread_id, thread_type = thread_type)
                             self.send(Message(u'點餐格式 : o <品名+細項> <數量> <價錢>'), thread_id = thread_id, thread_type = thread_type)
+                            self.now_shop.append(shop_name)
                         elif text_list[1] == 'total':
-                            with open('Source' + sep + 'tem_list.txt', 'r') as File:
-                                l = File.read().split('\n')
+                            with open('tem_list.txt', 'rb') as File:
+                                l = File.read().split(b'\n')
                                 l.sort()
                                 print('---order total---')
                                 for order in l:
-                                    print(order)
+                                    print(order.decode())
                                 print('---order total---')
                                 
                         elif text_list[1] == 'close':
-                            try:
-                                os.remove('Source' + sep + 'tem_list.txt')
-                            except:
-                                print('list has been removed')
-                            finally:
-                                self.send(Message(u'關閉點餐...'), thread_id = thread_id, thread_type = thread_type)
+                            if len(text_list) == 2:
+                                try:
+                                    self.now_shop.clear()
+                                    
+                                    with open('tem_list.txt', 'rb') as File:
+                                        l = File.read().split(b'\n')
+                                        l.sort()
+                                    print('---order total---')
+                                    for order in l:
+                                        print(order.decode())
+                                    print('---order total---')
+                                    
+                                    os.remove('tem_list.txt')
+                                except:
+                                    print('list has been removed')
+                                finally:
+                                    self.send(Message(u'關閉點餐...'), thread_id = thread_id, thread_type = thread_type)
+                            '''
+                            else:
+                                self.now_shop.remove(M.split(' ', 2)[2])
+                                self.send(Message('已停止訂購' + M.split(' ', 2)[2]), thread_id = thread_id, thread_type = thread_type)
+                            '''
                     #order is open, customer start to order
-                    elif self.order_open:
+                    elif self.order_open and len(text_list) == 4:
                         try:
                             buyer = (self.fetchUserInfo(author_id))[author_id]
                             product = text_list[1]
                             num = text_list[2]
                             money = text_list[3]
 
-                            print(buyer.last_name, buyer.first_name)
+                            print(buyer.first_name)
                         
-                            with open('Source' + sep + 'tem_list.txt', 'ab') as File:
+                            with open('tem_list.txt', 'ab') as File:
                                 File.write(' '.join([buyer.first_name, product, num + '份', money + '元', '\n']).encode('utf-8'))
                                 self.send(Message(u'點餐成功:)'), thread_id = thread_id, thread_type = thread_type)
                         except:
@@ -171,10 +192,18 @@ class Kavic_Bot(Client):
                     #order not open
                     elif not self.order_open:
                         self.send(Message(u'還沒開訂 哥'), thread_id = thread_id, thread_type = thread_type)
-                    
+                    else:
+                        self.send(Message(u'輸入錯誤喔:('), thread_id = thread_id, thread_type = thread_type)
                         
                 else:
                     self.send(Message(u'輸入錯誤喔ㄍ:('), thread_id = thread_id, thread_type = thread_type)
+                    
+            #send menu pic
+            if M.startswith('菜單') or M.startswith('menu'):
+                self.send(Message(u'現正訂購:'), thread_id = thread_id, thread_type = thread_type)
+                for shop in self.now_shop:
+                    self.sendLocalFiles('Source' + sep + 'shop' + sep + pc[shop], message = Message(shop), thread_id = thread_id, thread_type = thread_type)
+
                     
             #system commands, only admin can use these
             if author_id == self.uid:
